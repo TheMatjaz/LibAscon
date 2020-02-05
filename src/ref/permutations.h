f2f407fc1b8160b6a2d6244ffbe2cde7fcf7fed2
Matjaž <dev@matjaz.it>
2020-02-05 22:31:59 +0100

    Reorganise content of the internal header, without logical changes

--- src/ref/permutations.h
+++ src/ref/permutations.h
@@ -1,36 +1,58 @@
-#ifndef PERMUTATIONS_H_
-#define PERMUTATIONS_H_
+/**
+ * @file
+ * LibAscon internal header file.
+ *
+ * Common code (mostly the code state permutations) applied during
+ * encryption, decryption and hashing.
+ * 
+ * @license Creative Commons Zero (CC0) 1.0
+ * @authors see AUTHORS.md file
+ */
+
+#ifndef PERMUTATIONS_H
+#define PERMUTATIONS_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 
 #include <stddef.h>
 #include <stdint.h>
 #include "ascon.h"
 
-#define AEAD128_IV ( \
-     ((uint64_t)(8 * (ASCON_AEAD_KEY_LEN)) << 56U) \
-     | ((uint64_t)(8 * (ASCON_RATE)) << 48U) \
-     | ((uint64_t)(PA_ROUNDS) << 40U) \
-     | ((uint64_t)(PB_ROUNDS) << 32U) \
-     )
+#define PERMUTATION_A_ROUNDS 12
+#define PERMUTATION_B_ROUNDS 6
 #define XOF_IV ( \
     ((uint64_t)(8 * (ASCON_RATE)) << 48U) \
-    | ((uint64_t)(PA_ROUNDS) << 40U) \
+    | ((uint64_t)(PERMUTATION_A_ROUNDS) << 40U) \
     )
+#define AEAD128_IV ( \
+     ((uint64_t)(8 * (ASCON_AEAD_KEY_LEN)) << 56U) \
+     | XOF_IF \
+     | ((uint64_t)(PERMUTATION_B_ROUNDS) << 32U) \
+     )
 #define HASH_IV (XOF_IV | (uint64_t)(8 * ASCON_HASH_DIGEST_LEN))
 #define PADDING(bytes) (0x80ULL << (56 - 8 * ((size_t) bytes)))
-
-#define PA_ROUNDS 12
-#define PB_ROUNDS 6
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
 void inline printstate(const char* text, const ascon_state_t* state);
+
 uint64_t bytes_to_u64(const uint8_t* bytes, uint_fast8_t n);
+
 void u64_to_bytes(uint8_t* bytes, uint64_t x, uint_fast8_t n);
+
 uint64_t byte_mask(uint_fast8_t n);
+
 void ascon_permutation_a12(ascon_state_t* state);
+
 void ascon_permutation_8(ascon_state_t* state);
+
 void ascon_permutation_b6(ascon_state_t* state);
 
+#ifdef __cplusplus
+}
+#endif
 
-#endif  // PERMUTATIONS_H_
-
+#endif  /* PERMUTATIONS_H */
