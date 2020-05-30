4d29b6d3e3e3549454816f94213ddce992739a57
Matjaž <dev@matjaz.it>
2020-05-30 13:12:55 +0200

    Reach same performance as original implementation (!!)
    Basically LibAscon was already as performant as the original
    reference implementation, but the benchmarking test was flawed.
    Now the benchmarking in release mode produces identical results.
    
    For yet unknown reasons the rewritten benchmarking produces
    about 2x slower results. By replacing the whole benchmark.c
    with the content of the original and just calling the LibAscon
    function instead of the original one, the performances are
    exactly the same.

--- tst/benchmark.c
+++ tst/benchmark.c
@@ -2,213 +2,169 @@
  * @file
  * Benchmarking tool measuring the average CPU cycles per byte processed.
  *
+ * Copied from the original implementation, forced into usign CRYPTO_AEAD
+ * and calling the offline Ascon128 function from LibAscon instead of the
+ * original implementation.
+ *
  * @license Creative Commons Zero (CC0) 1.0
  * @authors see AUTHORS.md file
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <inttypes.h>
 #include "ascon.h"
 
-#if !defined(__arm__) && !defined(_M_ARM)
-    // Using RDTSC to count cycles
-    #ifdef _MSC_VER
-        #include <intrin.h>
-        #define ALIGN(x)
-    #else
-        #include <x86intrin.h>
-        #define ALIGN(x) __attribute__((aligned(x)))
-    #endif
-    #define init_cpucycles() do{}while(0)
-    #define cpucycles(cycles) cycles = __rdtsc()
-#endif
+#define CRYPTO_AEAD
+#define CRYPTO_KEYBYTES 16
+#define CRYPTO_NSECBYTES 0
+#define CRYPTO_NPUBBYTES 16
+#define CRYPTO_ABYTES 16
 
+
+#if !defined(__arm__) && !defined(_M_ARM)
+#pragma message("Using RDTSC to count cycles")
+#ifdef _MSC_VER
+#include <intrin.h>
+#define ALIGN(x)
+#else
+#include <x86intrin.h>
+#define ALIGN(x) __attribute__((aligned(x)))
+#endif
+#define init_cpucycles()
+#define cpucycles(cycles) cycles = __rdtsc()
+//#define cpucycles(cycles) cycles = __rdtscp(&tmp)
+#endif
 
 #if defined(__ARM_ARCH_6__) || __ARM_ARCH == 6 || _M_ARM == 6
-    #define ALIGN(x) __attribute__((aligned(x)))
-    #pragma message("Using ARMv6 PMU to count cycles")
-    #define init_cpucycles() \
-      __asm__ __volatile__("mcr p15, 0, %0, c15, c12, 0" ::"r"(1))
-    #define cpucycles(cycles) \
-      __asm__ __volatile__("mrc p15, 0, %0, c15, c12, 1" : "=r"(cycles))
+#define ALIGN(x) __attribute__((aligned(x)))
+#pragma message("Using ARMv6 PMU to count cycles")
+#define init_cpucycles() \
+  __asm__ __volatile__("mcr p15, 0, %0, c15, c12, 0" ::"r"(1))
+#define cpucycles(cycles) \
+  __asm__ __volatile__("mrc p15, 0, %0, c15, c12, 1" : "=r"(cycles))
 #elif defined(__arm__) || defined(_M_ARM)
-    #define ALIGN(x) __attribute__((aligned(x)))
-    #pragma message("Using ARMv7 PMU to count cycles")
-    #define init_cpucycles()                                                \
-      __asm__ __volatile__("mcr p15, 0, %0, c9, c12, 0" ::"r"(17));         \
-      __asm__ __volatile__("mcr p15, 0, %0, c9, c12, 1" ::"r"(0x8000000f)); \
-      __asm__ __volatile__("mcr p15, 0, %0, c9, c12, 3" ::"r"(0x8000000f))
-    #define cpucycles(cycles) \
-      __asm__ __volatile__("mrc p15, 0, %0, c9, c13, 0" : "=r"(cycles))
+#define ALIGN(x) __attribute__((aligned(x)))
+#pragma message("Using ARMv7 PMU to count cycles")
+#define init_cpucycles()                                                \
+  __asm__ __volatile__("mcr p15, 0, %0, c9, c12, 0" ::"r"(17));         \
+  __asm__ __volatile__("mcr p15, 0, %0, c9, c12, 1" ::"r"(0x8000000f)); \
+  __asm__ __volatile__("mcr p15, 0, %0, c9, c12, 3" ::"r"(0x8000000f))
+#define cpucycles(cycles) \
+  __asm__ __volatile__("mrc p15, 0, %0, c9, c13, 0" : "=r"(cycles))
 #endif
 
+#define NUM_RUNS 100
+#define NUM_BYTES 32768
+#define MAX_LEN 32768
+#define NUM_MLENS 7
+
+unsigned long long mlens[] = {1, 8, 16, 32, 64, 1536, 32768};
+unsigned char ALIGN(16) m[MAX_LEN];
+#if defined(CRYPTO_AEAD)
+unsigned long long alen = 0;
+unsigned long long clen = 0;
+unsigned char ALIGN(16) a[MAX_LEN];
+unsigned char ALIGN(16) c[MAX_LEN + CRYPTO_ABYTES];
+unsigned char ALIGN(16) nsec[CRYPTO_NSECBYTES ? CRYPTO_NSECBYTES : 1];
+unsigned char ALIGN(16) npub[CRYPTO_NPUBBYTES ? CRYPTO_NPUBBYTES : 1];
+unsigned char ALIGN(16) k[CRYPTO_KEYBYTES];
+#elif defined(CRYPTO_HASH)
+unsigned char ALIGN(16) h[CRYPTO_BYTES];
+#endif
+
+unsigned long long cycles[NUM_MLENS][NUM_RUNS * 2];
+unsigned int tmp;
+
+void init_input() {
+  int i;
+  for (i = 0; i < MAX_LEN; ++i) m[i] = rand();
+#if defined(CRYPTO_AEAD)
+  for (i = 0; i < MAX_LEN; ++i) a[i] = rand();
+  for (i = 0; i < CRYPTO_KEYBYTES; ++i) k[i] = rand();
+  for (i = 0; i < CRYPTO_NPUBBYTES; ++i) npub[i] = rand();
+#endif
+}
+
+unsigned long long measure(unsigned long long mlen) {
+  unsigned long long NREPS = NUM_BYTES / mlen;
+  unsigned long long i;
 #if defined(__arm__) || defined(_M_ARM)
-typedef unsigned int cycles_t;
+  unsigned int before, after;
 #else
-typedef uint64_t cycles_t;
+  unsigned long long before, after;
 #endif
-
-#define AMOUNT_OF_RUNS 16U
-#define MAX_TEXT_LEN 32768U
-#define AMOUNT_OF_TEXT_LENGTHS 7U
-
-const uint16_t TEXT_LENGTHS[] = {1, 8, 16, 32, 64, 1536, MAX_TEXT_LEN};
-
-typedef struct
-{
-    uint8_t ALIGN(16) text[MAX_TEXT_LEN];
-    uint8_t ALIGN(16) key[ASCON_AEAD128_KEY_LEN];
-    uint8_t ALIGN(16) nonce[ASCON_AEAD_NONCE_LEN];
-    uint8_t ALIGN(16) assoc_data[MAX_TEXT_LEN];
-    uint8_t ALIGN(16) obtained_tag[ASCON_AEAD_TAG_MIN_SECURE_LEN];
-    uint8_t ALIGN(16) obtained_digest[ASCON_HASH_DIGEST_LEN];
-    size_t text_len;
-} benchmark_data_t;
-
-static void init_random_array(uint8_t* array, size_t amount)
-{
-    while (amount--)
-    {
-        *(array++) = (uint8_t) rand();
-    }
+  init_input();
+  cpucycles(before);
+  for (i = 0; i < NREPS; ++i)
+#if defined(CRYPTO_AEAD)
+    ascon_aead128_encrypt(c,
+                          c + mlen,
+                          k,
+                          npub,
+                          NULL,
+                          m,
+                          0,
+                          mlen,
+                          16);
+#elif defined(CRYPTO_HASH)
+    crypto_hash(h, m, mlen);
+#endif
+  cpucycles(after);
+  return after - before;
 }
 
-static void init_benchmark_data(benchmark_data_t* const data,
-                                const size_t text_len)
-{
-    init_random_array(data->text, text_len);
-    init_random_array(data->key, ASCON_AEAD128_KEY_LEN);
-    init_random_array(data->nonce, ASCON_AEAD_NONCE_LEN);
-    init_random_array(data->assoc_data, text_len);
-    data->text_len = text_len;
+int compare_uint64(const void* first, const void* second) {
+  const unsigned long long* ia = (const unsigned long long*)first;
+  const unsigned long long* ib = (const unsigned long long*)second;
+  if (*ia > *ib) return 1;
+  if (*ia < *ib) return -1;
+  return 0;
 }
 
-static void benchmark_cycles(
-        uint64_t elapsed_cycles[AMOUNT_OF_TEXT_LENGTHS][AMOUNT_OF_RUNS * 2])
-{
-    benchmark_data_t data;
-    cycles_t start;
-    cycles_t end;
-    puts("Benchmarking...");
-    fflush(stdout);
-    init_cpucycles();
-    for (size_t len = 0; len < AMOUNT_OF_TEXT_LENGTHS; len++)
-    {
-        const uint64_t repetitions = MAX_TEXT_LEN / TEXT_LENGTHS[len];
-        for (size_t run = 0; run < AMOUNT_OF_RUNS; run++)
-        {
-            init_benchmark_data(&data, TEXT_LENGTHS[len]);
-            cpucycles(start);
-            for (size_t rep = 0; rep < repetitions; rep++)
-            {
-                ascon_aead128_encrypt(
-                        data.text,
-                        data.obtained_tag,
-                        data.key,
-                        data.nonce,
-                        data.assoc_data,
-                        data.text,
-                        data.text_len,
-                        data.text_len,
-                        ASCON_AEAD_TAG_MIN_SECURE_LEN);
-            }
-            cpucycles(end);
-            elapsed_cycles[len][run] = end - start;
-        }
-    }
-}
+int main(int argc, char* argv[]) {
+  unsigned long long i, j;
+  double factor = 1.0;
+  if (argc == 2) factor = atof(argv[1]);
 
-int compare_uint64(const void* const first, const void* const second)
-{
-    const uint64_t* const a = (uint64_t*) first;
-    const uint64_t* const b = (uint64_t*) second;
-    if (*a > *b) { return 1; }
-    if (*a < *b) { return -1; }
-    return 0;
-}
+  init_cpucycles();
 
-static void sort_cycles(
-        uint64_t elapsed_cycles[AMOUNT_OF_TEXT_LENGTHS][AMOUNT_OF_RUNS * 2])
-{
-    for (size_t len = 0; len < AMOUNT_OF_TEXT_LENGTHS; ++len)
-    {
-        qsort(elapsed_cycles[len], AMOUNT_OF_RUNS, sizeof(uint64_t),
-              &compare_uint64);
-    }
-}
+  for (i = 0; i < NUM_MLENS; ++i) {
+    for (j = 0; j < NUM_RUNS; ++j) cycles[i][j] = measure(mlens[i]);
+    qsort(cycles[i], NUM_RUNS, sizeof(unsigned long long), &compare_uint64);
+  }
 
-static void print_cycles(
-        const uint64_t elapsed_cycles
-        [AMOUNT_OF_TEXT_LENGTHS][AMOUNT_OF_RUNS * 2])
-{
-    printf("\nSorted cycles:\n");
-    for (size_t len = 0; len < AMOUNT_OF_TEXT_LENGTHS; len++)
-    {
-        const uint64_t repetitions = MAX_TEXT_LEN / TEXT_LENGTHS[len];
-        printf("%5u: ", TEXT_LENGTHS[len]);
-        for (size_t run = 0; run < AMOUNT_OF_RUNS; run++)
-        {
-            printf("%8"PRIu64" ", (elapsed_cycles[len][run] / repetitions));
-        }
-        printf("\n");
-    }
-}
+  printf("\nsorted cycles:\n");
+  for (i = 0; i < NUM_MLENS; ++i) {
+    unsigned long long NREPS = NUM_BYTES / mlens[i];
+    printf("%5d: ", (int)mlens[i]);
+    for (j = 0; j < NUM_RUNS; ++j) printf("%d ", (int)(cycles[i][j] / NREPS));
+    printf("\n");
+  }
 
-static void print_stats(
-        const uint64_t elapsed_cycles
-        [AMOUNT_OF_TEXT_LENGTHS][AMOUNT_OF_RUNS * 2],
-        const double factor)
-{
-    printf("\nCycles per byte (min, median):\n");
-    for (size_t len = 0; len < AMOUNT_OF_TEXT_LENGTHS; ++len)
-    {
-        uint64_t repetitions = MAX_TEXT_LEN / TEXT_LENGTHS[len];
-        uint64_t bytes = TEXT_LENGTHS[len] * repetitions;
-        printf("%5u: %6.1f %6.1f\n", TEXT_LENGTHS[len],
-               factor * elapsed_cycles[len][0] / bytes + 0.05,
-               factor * elapsed_cycles[len][AMOUNT_OF_RUNS / 2] / bytes + 0.05);
-    }
-    puts("");
-    for (size_t len = 0; len < AMOUNT_OF_TEXT_LENGTHS; len++)
-    {
-        printf("| %5u ", TEXT_LENGTHS[len]);
-    }
-    printf("|\n");
-    for (size_t len = 0; len < AMOUNT_OF_TEXT_LENGTHS; len++)
-    {
-        printf("|------:");
-    }
-    printf("|\n");
-    for (size_t len = 0; len < AMOUNT_OF_TEXT_LENGTHS; len++)
-    {
-        const uint64_t repetitions = MAX_TEXT_LEN / TEXT_LENGTHS[len];
-        const uint64_t bytes = TEXT_LENGTHS[len] * repetitions;
-        if (TEXT_LENGTHS[len] <= 32)
-        {
-            printf("| %5.0f ", factor * elapsed_cycles[len][0] / bytes + 0.5);
-        }
-        else
-        {
-            printf("| %5.1f ", factor * elapsed_cycles[len][0] / bytes + 0.05);
-        }
-    }
-    printf("|\n");
-}
+  printf("\ncycles per byte (min,median):\n");
+  for (i = 0; i < NUM_MLENS; ++i) {
+    unsigned long long NREPS = NUM_BYTES / mlens[i];
+    unsigned long long bytes = mlens[i] * NREPS;
+    printf("%5d: %6.1f %6.1f\n", (int)mlens[i],
+           factor * cycles[i][0] / bytes + 0.05,
+           factor * cycles[i][NUM_RUNS / 2] / bytes + 0.05);
+  }
+  printf("\n");
 
-int main(int argc, char* argv[])
-{
-    double factor = 1.0;
-    if (argc == 2)
-    {
-        factor = atof(argv[1]);
-    }
-    uint64_t elapsed_cycles[AMOUNT_OF_TEXT_LENGTHS][AMOUNT_OF_RUNS * 2];
-    benchmark_cycles(elapsed_cycles);
-    sort_cycles(elapsed_cycles);
-    print_cycles(elapsed_cycles);
-    print_stats(elapsed_cycles, factor);
-    return 0;
+  for (i = 0; i < NUM_MLENS; ++i) printf("| %5d ", (int)mlens[i]);
+  printf("|\n");
+  for (i = 0; i < NUM_MLENS; ++i) printf("|------:");
+  printf("|\n");
+  for (i = 0; i < NUM_MLENS; ++i) {
+    unsigned long long NREPS = NUM_BYTES / mlens[i];
+    unsigned long long bytes = mlens[i] * NREPS;
+    if (mlens[i] <= 32)
+      printf("| %5.0f ", factor * cycles[i][0] / bytes + 0.5);
+    else
+      printf("| %5.1f ", factor * cycles[i][0] / bytes + 0.05);
+  }
+  printf("|\n");
+
+  return 0;
 }
 
