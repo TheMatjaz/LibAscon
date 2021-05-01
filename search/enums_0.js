9721f239a63d3e5c38360f96f821f2c28a4b75a4
Matjaž Guštin <dev@matjaz.it>
2021-05-01 09:55:43 +0200

    Update GH-Pages to v1.0.2

--- search/enums_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['ascon_5ftag_5fvalidity_5ft_85',['ascon_tag_validity_t',['../ascon_8h.html#a6c5b7b4fcd00efe80f898e5f69ad8e17',1,'ascon.h']]]
-];
--- search/enumvalues_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['ascon_5ftag_5finvalid_86',['ASCON_TAG_INVALID',['../ascon_8h.html#a6c5b7b4fcd00efe80f898e5f69ad8e17a7a7b7badada7b92f70bad477f1ab53b3',1,'ascon.h']]],
-  ['ascon_5ftag_5fok_87',['ASCON_TAG_OK',['../ascon_8h.html#a6c5b7b4fcd00efe80f898e5f69ad8e17a7b37ec8ec5e2969d4aaa779707af9e24',1,'ascon.h']]]
-];
