72f9daa74ff98ff31abbd52812f201363f503daf
Matjaž Guštin <dev@matjaz.it>
2021-05-08 15:20:18 +0200

    Fix CL error about incorrect printf format string for size_t

--- tst/test_structs.c
+++ tst/test_structs.c
@@ -12,12 +12,12 @@
 
 void test_structs(void)
 {
-    printf("Sizeof sponge: %lu\n", sizeof(ascon_sponge_t));
+    printf("Sizeof sponge: %zu\n", sizeof(ascon_sponge_t));
     atto_eq(sizeof(ascon_sponge_t), 5 * 8);
-    printf("Sizeof bufstate: %lu\n", sizeof(ascon_bufstate_t));
+    printf("Sizeof bufstate: %zu\n", sizeof(ascon_bufstate_t));
     atto_eq(sizeof(ascon_bufstate_t), sizeof(ascon_sponge_t)
                                       + ASCON_DOUBLE_RATE + 8);
-    printf("Sizeof aead ctx: %lu\n", sizeof(ascon_aead_ctx_t));
+    printf("Sizeof aead ctx: %zu\n", sizeof(ascon_aead_ctx_t));
     atto_eq(sizeof(ascon_aead_ctx_t), sizeof(ascon_bufstate_t)
                                       + 8 + 8 + 8);
 }
